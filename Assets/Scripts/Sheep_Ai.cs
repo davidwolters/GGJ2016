e2e9ed34efc513691a1d146626bd83e98f40fb76
Alexander Björkman <you@example.com>
2016-01-29 20:34:16 +0100

    one thing

--- Assets/Scripts/Sheep_Ai.cs
+++ Assets/Scripts/Sheep_Ai.cs
@@ -3,89 +3,28 @@ using System.Collections;
 
 public class Sheep_Ai : MonoBehaviour {
 	
-	// The maximum time between turns.
-	public float turnMaxTime = 3;
-
-	// The minimum time between turns.
-	public float turnMinTime = 2;
-	
-	// Time untill the next rotate. Deincreaments every frame.
-	private float timeUntillRotate;
-	
-	// The speed of the AI.
-	[SerializeField] private float speed = 1; 
-	
-	// The area that the AI is not allowed to leave.
+	private NavMeshAgent agent;
 	[SerializeField] private GameObject arena;
-	
-	// Unneeded referance to rigid body.
-	private Rigidbody rg;
-	
-	// If we need to go to center.
-	private bool reroute = false;
-	
-	void Start () {
-	
-		// Grav the referance.
-		rg = gameObject.GetComponent<Rigidbody>();
-	}
-	
-	void OnTriggerExit (Collider coll)
+	void Start ()
 	{
-		// In case we are colliding with the border then go home.
-		if (coll.gameObject == arena)
-		{
-
-			// Look at the center so that we don't fall off.
-			gameObject.transform.LookAt(arena.transform);
-			
-			// We can't do normal movements anymore.
-			reroute = true;
-		}
+		agent = GetComponent <NavMeshAgent> ();
 	}
-	
-	void OnTriggerEnter (Collider coll)
-	{
-		// check if we are inside the arena again.
-		if (coll.gameObject == arena)
-		{
-			
-			// We can now continue with standard movements.
-			reroute = false;
-			
-			// To make sure that AI doesn't get stuck while trying to escape a border.
-			timeUntillRotate = turnMaxTime;
-		}
-	}
-	
-	// Update is called once per frame
 	void Update ()
 	{
-		// Check if rotate or go forward.
-		if (timeUntillRotate <=0)
-		{
-			
-			
-			// If we are not going outisde. Then rotate.
-			if (reroute != true)
-			{
-				// Calculate a new rotation.
-				int rota = Random.Range(0, 360);
-				
-				// Append the new rotation.
-				gameObject.transform.Rotate(0, rota, 0);
-			}
-			
-			// Calculate the next time we rotate.
-			timeUntillRotate = Random.Range (turnMinTime, turnMaxTime);
-		}
-		else
-		{  
-			// Continue going forward.
-			gameObject.transform.position+=transform.forward * Time.deltaTime * speed;
-			
-			// Decrease timer untill rotation.
-			timeUntillRotate-= Time.deltaTime;
-		}
+		Vector3 dest = RandomVector(arena.transform.position - (arena.transform.localScale), arena.transform.position + (arena.transform.localScale) );
+		agent.SetDestination(dest);
 	}
+	
+	
+	public static Vector3 RandomVector (Vector3 start, Vector3 end)
+	{
+		Vector3 v = new Vector3();
+		v.x = Random.Range(start.x, end.x);
+		v.y = Random.Range(start.y, end.y);
+		v.z = Random.Range(start.z, end.z);
+		return v;
+		
+      }
+
+	
 }
