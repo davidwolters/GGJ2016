7207e10d2f744a51d89ef9acdca5fa05d438dd14
David Wolters <david@Dags-MBP.Tekniskamuseet.se>
2016-01-29 22:19:41 +0100

    Working on grabbing sheep

---
+++ Assets/Scripts/Player_CarrySheep.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_CarrySheep : MonoBehaviour 
+{
+	[SerializeField] private Vector3 relativeSheepPos;
+	[SerializeField] private Vector3 relativeSheepRot;
+	[SerializeField] private Vector3 relativePutDownSheepPos;
+
+	private Quaternion relativeSheepQuat;
+
+	private Transform sheep;
+
+	public static bool carryingSheep = false;
+
+	private bool spaceDown = false;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		relativeSheepQuat = Quaternion.Euler (relativeSheepRot);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (sheep != null)
+		{
+			carryingSheep = true;
+			if (Input.GetAxisRaw (Player_Controls.sheepPickup) > 0)
+			{
+				sheep.localPosition = relativePutDownSheepPos;
+				sheep.SetParent (null);
+				sheep = null;
+				carryingSheep = false;
+			}
+		}
+
+		if (Input.GetAxisRaw (Player_Controls.sheepPickup) == 0)
+		{
+			spaceDown = false;
+		}
+	}
+
+	void OnCollisionStay (Collision coll)
+	{
+		if (coll.collider.tag == "Sheep")
+		{
+			if (Input.GetAxisRaw (Player_Controls.sheepPickup) > 0 && !spaceDown)
+			{
+				SetCarrySheep (coll.transform);
+				spaceDown = true;
+			}
+		}
+	}
+
+
+	public void SetCarrySheep (Transform sheep)
+	{
+		this.sheep = sheep;
+		this.sheep.SetParent (transform);
+		this.sheep.localPosition = relativeSheepPos;
+		this.sheep.localRotation = relativeSheepQuat;
+
+	}
+}
--- Assets/Scripts/Player_Controls.cs
+++ Assets/Scripts/Player_Controls.cs
@@ -5,6 +5,7 @@ public static class Player_Controls
 {
 	public static string horizontal = "Horizontal";
 	public static string vertical = "Vertical";
-	public static string shootWeb = "Fire1";
+	public static string shoot = "Fire1";
+	public static string sheepPickup = "Jump";
 
 }
