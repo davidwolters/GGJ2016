bc16ce79ea61d35484b7c9f4da145c85d006644c
Alexander Björkman <you@example.com>
2016-01-29 21:11:16 +0100

    Updated the sheep AI

--- Assets/Scripts/Sheep_Ai.cs
+++ Assets/Scripts/Sheep_Ai.cs
@@ -5,14 +5,31 @@ public class Sheep_Ai : MonoBehaviour {
 	
 	private NavMeshAgent agent;
 	[SerializeField] private GameObject arena;
+	
+	public float maxNewDestInterval = 4;
+	public float minNewDestInterval = 2;
+	
+	private float timeUntillNewDest = 0;
 	void Start ()
 	{
 		agent = GetComponent <NavMeshAgent> ();
 	}
 	void Update ()
 	{
-		Vector3 dest = RandomVector(arena.transform.position - (arena.transform.localScale), arena.transform.position + (arena.transform.localScale) );
-		agent.SetDestination(dest);
+		if (timeUntillNewDest <= 0)
+		{
+		
+			Vector3 dest = RandomVector(arena.transform.position - (arena.transform.localScale), arena.transform.position + (arena.transform.localScale) );
+			agent.SetDestination(dest);
+			timeUntillNewDest = Random.Range(minNewDestInterval ,maxNewDestInterval);
+		}
+		else
+		{
+			
+			timeUntillNewDest-=Time.deltaTime;
+			
+		}
+		//agent.
 	}
 	
 	
@@ -20,7 +37,7 @@ public class Sheep_Ai : MonoBehaviour {
 	{
 		Vector3 v = new Vector3();
 		v.x = Random.Range(start.x, end.x);
-		v.y = Random.Range(start.y, end.y);
+		//v.y = Random.Range(start.y, end.y);
 		v.z = Random.Range(start.z, end.z);
 		return v;
 		
