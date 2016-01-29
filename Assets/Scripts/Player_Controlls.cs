2beceeacdad8edd9fd572c6432d2251ea935d28f
David Wolters <david@Dags-MBP.Tekniskamuseet.se>
2016-01-29 19:35:29 +0100

    Added player movement

---
+++ Assets/Scripts/Player_Controlls.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Player_Controlls
+{
+	public static string horizontal = "Horizontal";
+	public static string vertical = "Vertical";
+
+}
---
+++ Assets/Scripts/Player_MovementScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_MovementScript : MonoBehaviour
+{
+	[SerializeField] private float moveSpeed;
+	[SerializeField] private Camera cam;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		UpdatePosition ();
+		UpdateRotation (); 
+
+	}
+
+	void UpdatePosition ()
+	{
+		float horMov = Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime;
+		float verMov = Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime;
+		transform.position += (horMov * Vector3.right + verMov * Vector3.forward);
+
+	}
+
+	void UpdateRotation ()
+	{
+		Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit))
+		{
+			Vector3 lookPoint = hit.point;
+			lookPoint.y = transform.position.y;
+			transform.LookAt (lookPoint);
+		}
+	}
+}
