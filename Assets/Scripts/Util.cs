526a23821beb224d3cdb97777e469e27ec921dec
sandcoder <david@wolters.se>
2016-01-30 10:23:09 +0100

    Deleted player oops

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -18,4 +18,31 @@ public static class Util
 			return Player_Webshoot.shooting2;	
 		}
 	}
+
+	public static void SetCarryingSheep (bool carrying, PlayerType player)
+	{
+		switch (player)
+		{
+		case PlayerType.PLAYER1:
+			Player_CarrySheep.carryingSheep1 = carrying;
+			break;
+		case PlayerType.PLAYER2:
+			Player_CarrySheep.carryingSheep2 = carrying;
+			break;
+		}
+	}
+
+	public static bool CarryingSheep (PlayerType player)
+	{
+		switch (player)
+		{
+		case PlayerType.PLAYER1:
+			return Player_CarrySheep.carryingSheep1;
+			break;
+		case PlayerType.PLAYER2:
+			return Player_CarrySheep.carryingSheep2;
+			break;
+		}
+		return false;
+	}
 }
