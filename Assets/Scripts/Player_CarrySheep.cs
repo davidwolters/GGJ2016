735521eb529a8ed5c98408cb2471f1d380154650
sandcoder <david@wolters.se>
2016-01-30 09:20:12 +0100

    Added fences and stuff

--- Assets/Scripts/Player_CarrySheep.cs
+++ Assets/Scripts/Player_CarrySheep.cs
@@ -7,12 +7,17 @@ public class Player_CarrySheep : MonoBehaviour
 	[SerializeField] private Vector3 relativeSheepRot;
 	[SerializeField] private Vector3 relativePutDownSheepPos;
 	[SerializeField] private PlayerType player;
+	[SerializeField] private float minTimeUntilDrop;
+	[SerializeField] private float maxTimeUntilDrop;
+
+	private float timeUntilDropTimer = 0;
 
 	private Quaternion relativeSheepQuat;
 
 	private Transform sheep;
 
-	public static bool carryingSheep = false;
+	public static bool carryingSheep1 = false;
+	public static bool carryingSheep2 = false;
 
 	private bool spaceDown = false;
 
@@ -21,6 +26,7 @@ public class Player_CarrySheep : MonoBehaviour
 	void Start ()
 	{
 		relativeSheepQuat = Quaternion.Euler (relativeSheepRot);
+
 	}
 	
 	// Update is called once per frame
@@ -31,17 +37,22 @@ public class Player_CarrySheep : MonoBehaviour
 			carryingSheep = true;
 			if (Input.GetKeyDown (Player_Controls.pickUpSheep (player)))
 			{
-				sheep.GetComponent <Sheep_Ai> ().enabled = true;
-				sheep.GetComponent <CapsuleCollider> ().enabled = true;
-				this.sheep.GetComponent <Rigidbody> ().isKinematic = false;
-				sheep.GetComponent <NavMeshAgent> ().enabled = true;
-
-
-
-				sheep.localPosition = relativePutDownSheepPos;
-				sheep.SetParent (null);
-				sheep = null;	carryingSheep = false;
+				DropSheep ();
 			}
+		}
+
+		if (timeUntilDropTimer > 0)
+		{
+			timeUntilDropTimer -= Time.deltaTime;
+			if (timeUntilDropTimer <= 0)
+			{
+				timeUntilDropTimer = 0;
+				if (sheep != null)
+				{
+					DropSheep ();
+				}
+			}
+
 		}
 
 
@@ -72,7 +83,21 @@ public class Player_CarrySheep : MonoBehaviour
 			this.sheep.GetComponent <Sheep_Ai> ().enabled = false;
 			this.sheep.GetComponent <Rigidbody> ().isKinematic = true;
 		}
+		timeUntilDropTimer = Random.Range (minTimeUntilDrop, maxTimeUntilDrop);
+	}
+
+	void DropSheep ()
+	{
+		sheep.GetComponent <Sheep_Ai> ().enabled = true;
+		sheep.GetComponent <CapsuleCollider> ().enabled = true;
+		this.sheep.GetComponent <Rigidbody> ().isKinematic = false;
+		sheep.GetComponent <NavMeshAgent> ().enabled = true;
 
 
+
+		sheep.localPosition = relativePutDownSheepPos;
+		sheep.SetParent (null);
+		sheep = null;	
+		carryingSheep = false;
 	}
 }
