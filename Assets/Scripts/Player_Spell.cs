0d82d354360961549368fd80e799898f20c2c161
Alexander Björkman <you@example.com>
2016-01-31 01:16:21 +0100

    Fixy

--- Assets/Scripts/Player_Spell.cs
+++ Assets/Scripts/Player_Spell.cs
@@ -13,28 +13,26 @@ public class Player_Spell : MonoBehaviour {
 	public GameObject playerSheep;
 	public GameObject enemySheep;
 
-	public GameObject playerArena;
-	public GameObject enemyArena;
+	public GameObject playerGround;
+	public GameObject enemyGround;
 	
 	public GameObject enemyObject;
 	
 	public GameObject crusherBlock;
 	
-	// Update is called once per frame
-	void Update () {
+	public SpellType spellType;
+	public enum SpellType {
+		
+		unknown,
+		freeze,
+		spawn,
+		crazy_sheeps,
+		wall_block
+		
+	}
 	
-		/*Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
-		if (Physics.Raycast(ray, out hit))
-		{
-			Vector3 lookPoint = hit.point;
-			//lookPoint.y = transform.position.y;
-			//transform.LookAt (lookPoint);
-			
-			
-			
-		}
-		*/
+	// For every key.
+	void Update () {
 		if (Input.GetKey(spell1))
 			DoSpell(0);
 		if (Input.GetKey(spell2))
@@ -49,15 +47,125 @@ public class Player_Spell : MonoBehaviour {
 	
 	void DoSpell(int spell)
 	{
-		
-		GameObject curse = (GameObject) Instantiate(Resources.Load("Curse"), transform.position, transform.rotation);
-		
-		curse.GetComponent<Spell_Curse>().GetRef (this.gameObject);
-		
-		Spell_Curse spell_curse = curse.GetComponent<Spell_Curse>();
-		
-		spell_curse.SetCurseById (spell);
-		
-		spell_curse.curser = this.gameObject;
+		SetCurseById(spell);
+		DoSpell();
 	}
+	
+	// Perform the spell / curse.
+	void DoSpell()
+	{
+	
+		if (spellType == SpellType.crazy_sheeps)
+			{
+				// Make sheeps crazy.
+				
+				GameObject [] sheeps = GameObject.FindGameObjectsWithTag(playerSheep.tag);
+				
+				foreach (GameObject element in sheeps)
+				{
+					// For each
+					element.GetComponent<Sheep_Ai>().Phase(2);
+				}
+				
+				return;
+				
+				
+			}
+			else if (spellType == SpellType.freeze)
+			{
+				
+				enemyObject.GetComponent<Player_Movement>().Freeze(7);
+				return;
+			}
+			else if (spellType == SpellType.spawn)
+			{
+			
+				print ("SPAWN");
+				Mesh mesh = enemyGround.GetComponent<MeshFilter>().sharedMesh;
+				
+				Vector3 center = (mesh.bounds.center) + enemyGround.transform.position;
+				
+			
+				GameObject sheep = (GameObject) Instantiate ( enemySheep, center, transform.rotation);
+				
+				
+				sheep.GetComponent<Sheep_Ai>().enabled = false;
+				sheep.GetComponent<Sheep_Ai>().arena = enemyGround;
+				sheep.GetComponent<Sheep_Ai>().enabled = true;
+				
+				
+				return;
+				
+			}
+			else if (spellType == SpellType.wall_block)
+			{
+				Mesh mesh = playerGround.GetComponent<MeshFilter>().sharedMesh;
+				//Max of arena
+				Vector3 max = TranslateBoundVec(mesh.bounds.max, playerGround.transform.lossyScale) + playerGround.transform.position;
+			
+				// Min of arena
+				Vector3 min = TranslateBoundVec(mesh.bounds.min, playerGround.transform.lossyScale) + playerGround.transform.position;
+			
+				
+				Vector3 pos = RandomVector (max, min);
+				
+				// DIRTY ALERT!
+				pos.z = 0;
+				
+				Instantiate (crusherBlock, pos, Quaternion.identity);
+				return;
+		
+			}
+	}
+	
+	// Set the curse/spell by id.	
+	public void SetCurseById( int id)
+	{
+		print ("Mana is: "+points.mana);
+		if (id == 0 && points.mana >= 5)
+		{
+			points.mana -= 1;
+			spellType = SpellType.crazy_sheeps;
+			return;
+		}
+		else if (id == 1 && points.mana >= 3)
+		{
+			points.mana -= 3;
+			spellType = SpellType.freeze;
+			return;
+		}
+		else if (id == 2 && points.mana >= 1)
+		{
+			points.mana -= 1;
+			spellType = SpellType.spawn;
+			return;
+		}
+		else if (id == 3 && points.mana >= 3)
+		{
+			points.mana -= 3;
+			spellType = SpellType.wall_block;
+			return;
+		}
+		
+	}
+	
+	// Good functions!
+	Vector3 TranslateBoundVec (Vector3 a, Vector3 b)
+	{
+		return new Vector3 (a.x * b.x, a.y * b.y, a.z * b.z);
+	}
+	
+	
+	public static Vector3 RandomVector (Vector3 start, Vector3 end)
+	{
+		Vector3 v = new Vector3();
+		v.x = Random.Range(start.x, end.x);
+		v.y = Random.Range(start.y, end.y);
+		v.z = Random.Range(start.z, end.z);
+		
+		
+		return v;
+		
+   	}
+	
 }
